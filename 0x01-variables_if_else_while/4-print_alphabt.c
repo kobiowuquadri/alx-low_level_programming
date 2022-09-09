7020c5b215936a1d21adf7f1138e8a9812a04447
kobiowuquadri <97671765+kobiowuquadri@users.noreply.github.com>
2022-09-09 23:25:07 +0100

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - print lowercase alpha a-z but remove 'q' and 'e'
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if ((letter != 'q') && (letter != 'e'))
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
