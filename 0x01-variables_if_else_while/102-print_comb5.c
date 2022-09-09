aee442cc6d478e1297b8c303466c9f55c5c27437
kobiowuquadri <97671765+kobiowuquadri@users.noreply.github.com>
2022-09-09 23:45:17 +0100

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+      int tens;
+      int units;
+      int t;
+      int u;
+
+      for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
+      {
+        for (units = '0'; units <= '9'; units++)
+        {
+          for (t = tens; t <= '9'; t++) /*print second of pair*/
+          {
+            for (u = units + 1; u <= '9'; u++)
+            {
+              putchar(tens);
+              putchar(units);
+              putchar(' ');
+              putchar(t);
+              putchar(u);
+
+              if (!((tens == '9' && units == '8') &&
+                    (t == '9' && u == '9')))
+              {
+                putchar(',');  /* inserting commas */
+                putchar(' ');
+              } /* end of IF statement */
+            }  /* end of last FOR loop */
+          }  /* end of  second to last loop */
+        }   /* end of the second loop */
+      }    /* end of  the nested loop */
+      putchar('\n');
+
+      return (0);
+  } /* end of  main */
