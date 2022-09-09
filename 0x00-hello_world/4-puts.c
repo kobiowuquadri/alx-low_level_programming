321e84d60dc790cec0ea04a32221ee520ea0c35f
Kobiowu quadri <kobiowuq@gmail.com>
2022-09-08 17:15:12 -0700

    my fifth commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/** main : script that prints content using the puts function
+ *
+ * Description: The program that print "Programming is like building a multilingual puzzle"
+ *return 0
+ */
+int main(void) 
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
