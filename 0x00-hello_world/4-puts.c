20aebda18b4102b90d4e7d6a560f8c34a96cc7ec
Kobiowu quadri <kobiowuq@gmail.com>
2022-09-08 17:54:49 -0700

    my fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,9 @@
  *
  * Description: Using the main function to print a content
  * The program that print "Programming is like building a multilingual puzzle"
- * Return: 0
+ * Return: Always 0(Success)
  */
+
 int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
