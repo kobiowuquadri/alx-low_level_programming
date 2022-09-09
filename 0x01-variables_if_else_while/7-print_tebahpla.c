4109a6b42ccefeff2c70c382458901e9b5f28512
kobiowuquadri <97671765+kobiowuquadri@users.noreply.github.com>
2022-09-09 23:29:57 +0100

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - print alphabet reversed using putchar
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet = 'z';
+
+	while (alphabet >= 'a')
+	{
+		putchar(alphabet);
+		alphabet--;
+	}
+	putchar('\n');
+
+	return (0);
+}
