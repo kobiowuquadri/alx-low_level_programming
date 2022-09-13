67e09dac99aedce28a8f111044c32da93eb19687
Kobiowu quadri <kobiowuq@gmail.com>
2022-09-13 08:52:14 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that print abc
+ *
+ * Return: 0
+ */
+
+void print_alphabet(viod)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
